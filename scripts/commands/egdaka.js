8642d378d7f6a1ad4460571ebb964b68036f5e8b
─꯭─⃝͎̽𓆩𝑫𝑬𝑽𝑰𝑳 𝑭𝑨𝑹'𝑯𝑨𝑵‣᭄𓆪___//😈🩵🪽 <tftanha06@gmail.com>
2025-07-15 01:25:47 +0600

    Update egdaka.js

--- scripts/commands/egdaka.js
+++ scripts/commands/egdaka.js
@@ -1,11 +1,11 @@
 const fs = require("fs");
 module.exports = {
   config:{
-  name: "🙋‍♀️",
+  name: "@─꯭─⃝͎̽C⃪͓͢T⃔͢ B̷⃪⃨͢O⃔͢T̷⃪▁▁▁╱╱😈🙂🩵🪽",
         version: "1.0.1",
         prefix: false,
   permssion: 0,
-  credits: "nayan", 
+  credits: "Farhan", 
   description: "Fun",
   category: "no prefix",
   usages: "🙋‍♂️",
@@ -16,7 +16,7 @@ handleEvent: function({ api, event, client, __GLOBAL }) {
   var { threadID, messageID } = event;
   const content = event.body ? event.body : '';
   const body = content.toLowerCase();
-  if (body.indexOf("oi")==0 || body.indexOf("oii")==0 || body.indexOf("🙋‍♀️")==0 || body.indexOf("🙋‍♂️")==0) {
+  if (body.indexOf("oi")==0 || body.indexOf("oii")==0 || body.indexOf("@কে'ট'বে'রি ত্যা'হ")==0 || body.indexOf("@─꯭─⃝͎̽C⃪͓͢T⃔͢ B̷⃪⃨͢O⃔͢T̷⃪▁▁▁╱╱😈🙂🩵🪽")==0) {
     var msg = {
         body: "~ ডাকো কেনো গো, আমি আছি তো এখানে..!✋🥀",
         attachment: fs.createReadStream(__dirname + `/Nayan/dakoknogo.mp3`)
