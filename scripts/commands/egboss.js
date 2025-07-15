9e2e95d4e6c881658c2a0dc24a79b5f810ecbe8f
─꯭─⃝͎̽𓆩𝑫𝑬𝑽𝑰𝑳 𝑭𝑨𝑹'𝑯𝑨𝑵‣᭄𓆪___//😈🩵🪽 <tftanha06@gmail.com>
2025-07-15 09:57:34 +0600

    Update egboss.js

--- scripts/commands/egboss.js
+++ scripts/commands/egboss.js
@@ -5,7 +5,7 @@ module.exports = {
         version: "1.0.1",
         prefix: false,
   permssion: 0,
-  credits: "nayan", 
+  credits: "Farhan", 
   description: "Fun",
   category: "no prefix",
   usages: "farhan",
@@ -18,7 +18,7 @@ handleEvent: function({ api, event, client, __GLOBAL }) {
   const body = content.toLowerCase();
   if (body.indexOf("Farhan")==0 || body.indexOf("FARHAN")==0 || body.indexOf("ফারহান")==0 || body.indexOf("farhan")==0) {
     var msg = {
-        body: "👉আমার বস♻️ 𝐑𝐉 𝐅𝐀𝐑𝐇𝐀𝐍 এখন বিজি আছে । তার ইনবক্সে এ মেসেজ দিয়ে রাখো ‎‎‎‎‎‎‎‎‎https://www.facebook.com/profile.php?id=61550121814016&mibextid=eBUYbo🔰 ♪√বস ফ্রি হলে আসবে🧡😁😜🐒",
+        body: "👉আমার বস♻️ 𝐑𝐉 𝐅𝐀𝐑𝐇𝐀𝐍 এখন বিজি আছে । তার ইনবক্সে এ মেসেজ দিয়ে রাখো ‎‎‎‎‎‎‎‎‎https://www.facebook.com/devil.rj.farhan!!♻️✔️ ♪√বস ফ্রি হলে আসবে🧡😁😜🐒",
         attachment: fs.createReadStream(__dirname + `/Nayan/boss.mp3`)
       }
       api.sendMessage( msg, threadID, messageID);
